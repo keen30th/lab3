79670892a5b1b51955dae954879eed3d396bb5bc
Keen <118066621+keen30th@users.noreply.github.com>
2023-10-25 17:31:27 +0300

    Add files via upload

---
+++ Models/Book.cs
@@ -0,0 +1,11 @@
+﻿namespace onta_nicusor_lab2.Models
+{
+    public class Book
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public decimal Price { get; set; }
+        public ICollection<Order> Orders { get; set; }
+    }
+}
